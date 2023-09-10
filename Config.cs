d67818c54af9804462a9dbc4ddbcc07c05a38b6a
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-09-11 01:43:09 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace H_W_Mod3_7
+{
+    public class Config
+    {
+        public int N { get; set; }
+
+        public static Config ReadFromJsonFile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Config>(json);
+        }
+    }
+}
